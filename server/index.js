5e855264d61d3fb69877d568e3dc3933504f61ea
Marissa Romero <marissakarenromero@gmail.com>
2021-04-01 17:54:22 -0700

    Debugging for netlify.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use('/', express.static(path.resolve(__dirname, './client/dist')));
+app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
 
 app.listen(3000, () => {
   console.log('listening on port 3000!');
