ae1a0e36867d8ec1832c0ed363de0194d9a29935
Marissa Romero <marissakarenromero@gmail.com>
2021-04-01 18:42:24 -0700

    Added a Procfile

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const path = require('path');
+let port = process.env.PORT;
+
+if (port == null || port === '') {
+  port = 8000;
+}
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,6 +12,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
 
-app.listen(3000, () => {
-  console.log('listening on port 3000!');
+app.listen(port, () => {
+  console.log('listening on port ', port);
 });
